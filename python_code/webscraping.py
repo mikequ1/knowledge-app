66aea272d0161f6ab9fc3c62ff0140cf5601c414
Conspiciens <fernandezjohann08@gmail.com>
2021-03-23 11:38:21 -0700

    Python Get Wiki Page and json file made

--- python_code/webscraping.py
+++ python_code/webscraping.py
@@ -1,12 +1,31 @@
 import requests 
 from bs4 import BeautifulSoup 
+import random
+import wikipedia
 
-topics = [] 
+topics = ["Communism", "Abortion", "Black Lives Matter", "Gun Control", "Climate Change", "Immigration", "Death Penalty", "Torture", "Andriod vs IOS", "Government Control", 
+        "U.S leave the middle east", "Best Sandwich", "Do IVY league matters", "Laws against Hate Speech", "Affirmative action", "Fracking", "defunding police", "marijuanas",
+        "Socialsim vs Capitalism"]
+
 wikipedia_link = "https://en.wikipedia.org/wiki/"
 
-def wiki_scraping(topic): 
-    page = request.get(wikipedia_link + topic).content 
+def choose_topics():
+    topic = random.choice(topics)
+    get_wikipedia(topic)
 
-    javascript_info = BeautifulSoup(page, features='lxml')
-    article = info.find_all()
 
+def get_wikipedia(topic):
+    #Get page of Wikipedia topic to get the title 
+    topic_start = wikipedia.page(topic)
+    print(topic_start.title)
+
+    #Get URL from Wikipedia Topic 
+    print(topic_start.url)
+
+    #Get a quick summary of the topic
+    print (wikipedia.summary(topic))
+    
+
+
+
+choose_topics()
