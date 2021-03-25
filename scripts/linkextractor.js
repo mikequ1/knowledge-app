b2e6b5a8c149e4120f4fa0f666752b25d73ce9c6
mikequ1 <mikequmrsh@gmail.com>
2021-03-24 17:11:45 -0700

    added some (ajax code)

--- scripts/linkextractor.js
+++ scripts/linkextractor.js
@@ -1,3 +1,21 @@
+var xhr = new XMLHttpRequest();
+var url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='New_England_Patriots'";
+xhr.open('GET', url, true);
+xhr.onload = function() {
+    var data = JSON.parse(this.response);
+    console.log(data);
+
+    // Log the page objects
+    console.log(data.query.pages)
+
+    for (var i in data.query.pages) 
+    {
+        console.log(data.query.pages[i].title);
+    }
+}
+xhr.send();
+
+
 const container = document.querySelector("#mw-pages");
 var x = container.querySelectorAll("a");
 var myarray = []
