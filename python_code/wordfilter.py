c66a912ca65bd2719dfb318d3d274d02f4c48963
mikequ1 <mikequmrsh@gmail.com>
2021-03-26 12:00:47 -0700

    cards load on page load index.html

--- python_code/wordfilter.py
+++ python_code/wordfilter.py
@@ -3,4 +3,5 @@ from profanity_check import predict, predict_prob
 def checkstr(str):
     return predict([str])
 
-print(checkstr('document'))
+print(checkstr('johann sucks'))
+#pip install alt-profanity-check
