2c43312320c9895f888bf42f2ca42dde6e95f586
mikequ1 <mikequmrsh@gmail.com>
2021-03-27 17:28:10 -0700

    updating front end interface

--- scripts/client.js
+++ scripts/client.js
@@ -31,3 +31,21 @@ function populateArticles(){
         document.getElementById("link"+(i+1)).href = thisLink;
     }
 }
+
+function pickCardBackground(i){
+    var bgArr = [];
+    bgArr.push("url(https://image.freepik.com/free-photo/abstract-pink-tone-lights-background-blurred-background_3248-2991.jpg)");
+    bgArr.push("url(https://countryclubhills.org/wp-content/uploads/2018/09/Background-Images-2.jpg)")
+    bgArr.push("url(https://thewallpaper.co/wp-content/uploads/2020/07/light-green-abstract-wallpaper-hd-background-wallpapers-free-amazing-tablet-smart-phone-4k-high-definition-1920x1080-1.jpg)");
+    bgArr.push("url(https://i.ytimg.com/vi/X8FVsLUvWTY/maxresdefault.jpg)");
+    bgArr.push("url(https://eskipaper.com/images/light-blue-backgrounds-7.jpg)");
+    bgArr.push("url(https://i.pinimg.com/originals/34/04/8d/34048d8aa063b17d2ac45510a4fc6466.jpg)");
+    bgArr.push("url(https://www.wallpaperbetter.com/wallpaper/797/61/130/light-abstract-smoke-1080P-wallpaper.jpg)");
+    bgArr.push("url(https://www.wallpapertip.com/wmimgs/149-1491561_sky-blue-hd-background-image-for-banner-light.jpg)");
+    bgArr.push("url(https://i.pinimg.com/originals/ef/9d/1a/ef9d1a5386d76e1a202034b3c1e92117.png)");
+
+    var yep = "url(https://imgur.com/a/uK3YjwC)"
+
+    var index = Math.floor(Math.random() * bgArr.length);
+    document.getElementById("card" + (i + 1)).style.backgroundImage = bgArr[index];
+}
